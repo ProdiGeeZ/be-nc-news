7ca77ad670aa70615542740ba8af55a68a0d5029
Saabir <82912568+ProdiGeeZ@users.noreply.github.com>
2023-11-20 15:31:44 +0000

    combine tests into one test file

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -15,10 +15,28 @@ afterAll(() => {
 describe("GET /api/nonexistentendpoint", () => {
     test("404: Responds with an error when endpoint path does not exist", () => {
         return request(app)
-        .get("/api/nonexistentendpoint")
-        .expect(404)
-        .then(({ body }) => {
-        expect(body.msg).toBe("Path not found");
-        });
+            .get("/api/nonexistentendpoint")
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.msg).toBe("Path not found");
+            });
+    });
+});
+
+describe("GET /api/topics", () => {
+    test("200: Should return with an array of topic objects, with slug and description properties", () => {
+        return request(app)
+            .get("/api/topics")
+            .expect(200)
+            .then(({ body }) => {
+                const topics = body.topics;
+                expect(topics).toHaveLength(3);
+                topics.forEach((topic) => {
+                    expect(topic).toMatchObject({
+                        slug: expect.any(String),
+                        description: expect.any(String),
+                    });
+                });
+            });
     });
 });
