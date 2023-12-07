16fae93bee32c13d113870f6432af5fe2c8dbd03
Saabir <82912568+ProdiGeeZ@users.noreply.github.com>
2023-12-07 20:50:38 +0000

    add configurations for seeding cloud database

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,21 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'production';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && process.env.DATABASE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
+
 
 const config = {};
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
+  config.ssl = { rejectUnauthorized: false };
   config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
