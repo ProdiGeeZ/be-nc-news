5e459dd7050170bd3f3da2817808e90a7ae897ea
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-05-02 17:52:47 +0100

    update docs for get comments

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -227,7 +227,7 @@ describe('GET /api/articles', () => {
     });
 });
 
-describe.only('GET /api/articles/:article_id/comments', () => {
+describe('GET /api/articles/:article_id/comments', () => {
     test('200: Should return an array of comments associated with the given article_id', () => {
         return request(app)
             .get("/api/articles/1/comments")
